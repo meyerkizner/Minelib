11af69e6969b8a26779d8321ce1982f3a1efc149
Ty Overby <ty@pre-alpha>
2011-06-16 21:38:15 -0700

    code doesn't work, but I think it should go something like this

---
+++ src/com/prealpha/minelib/world/Region.java
@@ -0,0 +1,35 @@
+/*
+ * MineLib, a Minecraft library
+ * Copyright (C) 2011 Meyer Kizner
+ * 
+ * This file is part of MineLib.
+ * 
+ * MineLib is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * MineLib is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * MineLib. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.prealpha.minelib.world;
+
+import java.nio.ByteBuffer;
+
+public class Region {
+	public Region(ByteBuffer bb){
+		//work your magic
+	}
+	
+	public Chunk getChunk(int x, int z){
+		return //the chunk;
+	}
+	public boolean chunkExists(int x, int z){
+		return //chunk exists
+	}
+}
---
+++ src/com/prealpha/minelib/world/World.java
@@ -0,0 +1,36 @@
+/*
+ * MineLib, a Minecraft library
+ * Copyright (C) 2011 Meyer Kizner
+ * 
+ * This file is part of MineLib.
+ * 
+ * MineLib is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * MineLib is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * MineLib. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.prealpha.minelib.world;
+
+import java.io.File;
+
+public class World {
+	public World(File worldDirectory){
+		//Load some stuff
+	}
+	
+	public Region getRegion(int x, int z){
+		return //the region at x,z
+	}
+	
+	public boolean regionExists(int x, int z){
+		return //region exists
+	}
+}
