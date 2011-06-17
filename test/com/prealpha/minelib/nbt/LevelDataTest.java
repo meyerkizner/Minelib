ce1566f2e305576698197e7b99903878987a26ec
Ty Overby <ty@pre-alpha>
2011-06-16 22:15:40 -0700

    added a new test to see if modifying an nbt file works, and if it gets translated into bytes correctly

---
+++ test/com/prealpha/minelib/nbt/LevelDataTest.java
@@ -0,0 +1,73 @@
+/*
+ * MineLib, a Minecraft library
+ * Copyright (C) 2011 Meyer Kizner
+ * 
+ * This file is part of MineLib.
+ * 
+ * MineLib is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * MineLib is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * MineLib. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.prealpha.minelib.nbt;
+
+import static org.junit.Assert.*;
+
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.util.zip.GZIPInputStream;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.google.common.io.ByteStreams;
+
+public class LevelDataTest {
+	private InputStream fileStream;
+	private ByteBuffer decompressedByteBuffer;
+
+	@Before
+	public void setUp() throws Exception {
+		// new random access file in read-only mode
+		fileStream = getClass().getResourceAsStream("level.dat");
+		byte[] data = ByteStreams.toByteArray(new GZIPInputStream(fileStream));
+		decompressedByteBuffer = ByteBuffer.wrap(data);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		if (fileStream != null) {
+			fileStream.close();
+		}
+	}
+
+	@Test
+	public void TestGzip() throws Exception {
+		NbtDecoder decoder = new NbtDecoder();
+		RootTag root = decoder.apply(decompressedByteBuffer);
+		//get the compound tag representing the player
+		CompoundTag playerCompoundTag = (CompoundTag)((CompoundTag)root.getValue().getValue().get("Data")).getValue().get("Player");
+		//makes sure that it is 20 (the value in the file)
+		assertEquals((short)20,playerCompoundTag.getValue().get("Health").getValue());
+		//set it equal to 10 now
+		playerCompoundTag.getValue().put("Health",new ShortTag((short) 10));
+		//check to see if the object now contains 10
+		assertEquals((short)10,playerCompoundTag.getValue().get("Health").getValue());
+		
+		//make a new root tag out of the old bytes to see if the values transfer over
+		RootTag newRoot = decoder.apply(root.toBytes());
+		//reassign the player compound tag to the new one
+		playerCompoundTag = (CompoundTag)((CompoundTag)newRoot.getValue().getValue().get("Data")).getValue().get("Player");
+		//see if the value carried over
+		assertEquals((short)10,playerCompoundTag.getValue().get("Health").getValue());
+	}
+}
